86b415be0c6e702bcd5d56b3918bd3f0da1e7f88
ToniIvars <toniivaju@gmail.com>
2021-04-04 21:53:31 +0200

    Added the functionality of the login view

--- eCommerce/authentication/views.py
+++ eCommerce/authentication/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
-def login(request):
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse(f'Logged in as {user}')
+        else:
+            messages.error(request, 'The username or the password are incorrect.')
+
     return render(request, 'authentication/login.html')
