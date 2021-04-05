7528ab1c9e56db3b4472d89a01202e27715d1552
ToniIvars <toniivaju@gmail.com>
2021-04-05 09:49:57 +0200

    Store app created

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'authentication'
+    'authentication',
+    'store'
 ]
 
 MIDDLEWARE = [
