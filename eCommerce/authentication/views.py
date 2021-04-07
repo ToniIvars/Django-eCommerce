d49ac1e93c3ff6e75a59ed08f506d9b317885eef
ToniIvars <toniivaju@gmail.com>
2021-04-07 08:24:47 +0200

    Login form done using django forms

--- eCommerce/authentication/views.py
+++ eCommerce/authentication/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from .forms import LoginForm
 
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        else:
-            messages.error(request, 'The username or the password are incorrect.')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.error(request, 'The username or the password are incorrect.')
     
     elif request.user.is_authenticated:
         return redirect('index')
 
-    return render(request, 'authentication/login.html')
+    else:
+        form = LoginForm()
+
+    return render(request, 'authentication/login.html', {'form':form})
 
 @login_required
 def logout_view(request):
