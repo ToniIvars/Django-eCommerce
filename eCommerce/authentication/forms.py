06e954d17b6fc85b0dc0037fe6a8ebbe50f857d2
ToniIvars <toniivaju@gmail.com>
2021-04-07 08:38:16 +0200

    Form styled with bootstrap

--- eCommerce/authentication/forms.py
+++ eCommerce/authentication/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Username'}))
-    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Username', 'class':'form-control'}))
+    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder':'Password', 'class':'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = ""
+        self.fields['password'].label = ""
