50ee7eff6542c401c37d3fde637028ca813714ad
ToniIvars <toniivaju@gmail.com>
2021-04-08 21:57:30 +0200

    Added the profile view, url and form

---
+++ eCommerce/store/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class ProfileForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username', 'class':'form-control'}))
+    password = forms.CharField(required=False, widget=forms.PasswordInput(attrs={'placeholder':'Password (not required)', 'class':'form-control'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Email', 'class':'form-control'}))
+    first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'First name', 'class':'form-control'}))
+    last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'Last name', 'class':'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        user_id = kwargs.pop('user_id', None)
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+        if user_id:
+            user = User.objects.get(id=user_id)
+
+            self.fields['username'].initial = user.username
+            self.fields['password'].initial = user.password
+            self.fields['email'].initial = user.email
+            self.fields['first_name'].initial = user.first_name
+            self.fields['last_name'].initial = user.last_name
+        
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        for field in fields:
+            self.fields[field].label = ''
--- eCommerce/store/views.py
+++ eCommerce/store/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import ProfileForm
 
 # Create your views here.
 @login_required
 def index(request):
     return render(request, 'store/index.html')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+
+            print(username, password, email, first_name, last_name)
+
+            messages.success('Profile edited successfully.')
+            return redirect('index')
+
+    else:
+        form = ProfileForm(user_id=request.user.id)
+
+    return render(request, 'store/profile.html', {'form':form})
