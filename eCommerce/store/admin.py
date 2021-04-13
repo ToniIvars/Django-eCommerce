9f2d7fdbd9f350acfc44f4271971543ebe44d5d6
ToniIvars <toniivaju@gmail.com>
2021-04-13 19:57:22 +0200

    Added the models for the Product and the Order

--- eCommerce/store/admin.py
+++ eCommerce/store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Order
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'seller', 'price')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'buyer')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
--- eCommerce/store/models.py
+++ eCommerce/store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seller')
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product')
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='buyer')
+    date = models.DateField(auto_now_add=True, editable=False, blank=True)
+
+    def __str__(self):
+        return f'{self.product}: {self.buyer}'
