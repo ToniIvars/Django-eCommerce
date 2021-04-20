760dab52c7863a2d6b64fac5fc21512f932c9922
ToniIvars <toniivaju@gmail.com>
2021-04-20 20:27:02 +0200

    Added buttons for the filters

--- eCommerce/static/store/base.js
+++ eCommerce/static/store/base.js
@@ -9,4 +9,19 @@ document.addEventListener('DOMContentLoaded', function () {
     button.onmouseleave = () => {
         image.src = '/static/store/search.svg'
     }
+
+    // Code for the filter buttons
+    const price = document.getElementById('price')
+    const price_reversed = document.getElementById('price_reversed')
+    const query = document.getElementById('query_hidden').innerText
+
+    const base_url = window.location.href.split('?')[0]
+
+    price.onclick = () => {
+        location.assign(`${base_url}?q=${query}&f=price`)
+    }
+
+    price_reversed.onclick = () => {
+        location.assign(`${base_url}?q=${query}&f=price_reversed`)
+    }
 })
