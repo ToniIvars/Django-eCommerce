0f044df111b132faa885b2a5dfe99b2b8e6ea6a9
ToniIvars <toniivaju@gmail.com>
2021-04-20 22:23:47 +0200

    Added a 'disabled' color when the filter is the one you are using

--- eCommerce/static/store/base.js
+++ eCommerce/static/store/base.js
@@ -15,13 +15,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const price_reversed = document.getElementById('price_reversed')
     const query = document.getElementById('query_hidden').innerText
 
-    const base_url = window.location.href.split('?')[0]
+    const url = window.location.href
+    const base_url = url.split('?')[0]
 
     price.onclick = () => {
         location.assign(`${base_url}?q=${query}&f=price`)
     }
-
     price_reversed.onclick = () => {
         location.assign(`${base_url}?q=${query}&f=price_reversed`)
+    }
+
+    if (url.includes('price_reversed')) {
+        price_reversed.disabled = true
+        price_reversed.classList.add('button-disabled')
+
+    } else if (url.includes('price')) {
+        price.disabled = true
+        price.classList.add('button-disabled')
     }
 })
