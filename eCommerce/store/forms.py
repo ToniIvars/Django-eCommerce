986ca7ae838ac62c3c6fe8d49c297f69c6ea850d
ToniIvars <toniivaju@gmail.com>
2021-04-21 20:32:01 +0200

    Chnaged the way of hiding the labels in ProfileForm

--- eCommerce/store/forms.py
+++ eCommerce/store/forms.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from .models import Product
 
 class ProfileForm(forms.Form):
-    password = forms.CharField(required=False, widget=forms.PasswordInput(attrs={'placeholder':'Password (not required)', 'class':'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'Email', 'class':'form-control'}))
-    first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'First name', 'class':'form-control'}))
-    last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder':'Last name', 'class':'form-control'}))
+    password = forms.CharField(label='', required=False, widget=forms.PasswordInput(attrs={'placeholder':'Password (not required)', 'class':'form-control'}))
+    email = forms.EmailField(label='', widget=forms.EmailInput(attrs={'placeholder':'Email', 'class':'form-control'}))
+    first_name = forms.CharField(label='', required=False, widget=forms.TextInput(attrs={'placeholder':'First name', 'class':'form-control'}))
+    last_name = forms.CharField(label='', required=False, widget=forms.TextInput(attrs={'placeholder':'Last name', 'class':'form-control'}))
 
     def __init__(self, *args, **kwargs):
         user_id = kwargs.pop('user_id', None)
@@ -15,14 +15,9 @@ class ProfileForm(forms.Form):
         if user_id:
             user = User.objects.get(id=user_id)
 
-            self.fields['password'].initial = user.password
             self.fields['email'].initial = user.email
             self.fields['first_name'].initial = user.first_name
             self.fields['last_name'].initial = user.last_name
-        
-        fields = ('password', 'email', 'first_name', 'last_name')
-        for field in fields:
-            self.fields[field].label = ''
 
 class ProductForm(forms.ModelForm): 
     class Meta:
