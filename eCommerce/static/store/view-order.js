add8bb06c2756082e5f1e9f8725ef295489ef513
ToniIvars <toniivaju@gmail.com>
2021-05-04 22:34:53 +0200

    Changed the design of the table

--- eCommerce/static/store/view-order.js
+++ eCommerce/static/store/view-order.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const state = document.getElementById('order-state')
 
     if (state.innerHTML === 'Opened') {
-        state.classList.add('green')
+        state.classList.add('red')
 
     } else if (state.innerHTML === 'In progress') {
         state.classList.add('yellow')
 
     } else if (state.innerHTML === 'Delivered') {
-        state.classList.add('red')
+        state.classList.add('green')
     }
 
 })
