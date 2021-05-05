bda735adaadc695b57e0a979ef0da35a8612cb3d
ToniIvars <toniivaju@gmail.com>
2021-05-05 20:01:54 +0200

    Added a dropdown to change the state of the delivery (only frontend at the moment)

--- eCommerce/static/store/view-order.js
+++ eCommerce/static/store/view-order.js
@@ -1,14 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const state = document.getElementById('order-state')
+    const first_item = Array.from(document.querySelectorAll('button.dropdown-toggle')).slice(-1)
+    const other_items = Array.from(document.querySelectorAll('button.dropdown-item'))
 
-    if (state.innerHTML === 'Opened') {
-        state.classList.add('red')
+    const items = first_item.concat(other_items)
 
-    } else if (state.innerHTML === 'In progress') {
-        state.classList.add('yellow')
-
-    } else if (state.innerHTML === 'Delivered') {
-        state.classList.add('green')
+    function check_state(element) {
+        if (element.innerHTML.includes('Opened')) {
+            return 'red'
+    
+        } else if (element.innerHTML.includes('In progress')) {
+            return 'yellow'
+    
+        } else if (element.innerHTML.includes('Delivered')) {
+            return 'green'
+        }
     }
+    
+    items.forEach(element => {
+        element.classList.add(check_state(element))
+    });
 
+    items.slice(1,3).forEach(element => {
+        element.onclick = () => {
+            let item1_text = items[0].innerHTML
+            let item1_color = check_state(items[0])
+            let element_color = check_state(element)
+
+            items[0].innerHTML = element.innerHTML
+            element.innerHTML = item1_text
+
+            items[0].classList.replace(item1_color, check_state(items[0]))
+            element.classList.replace(element_color, check_state(element))
+        }
+    });
 })
