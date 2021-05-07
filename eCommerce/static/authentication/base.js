1326bb07c506f644a1b803cfd91163398d148136
ToniIvars <toniivaju@gmail.com>
2021-05-07 21:27:49 +0200

    Added favicon and changing it depending on the browser theme

--- eCommerce/static/authentication/base.js
+++ eCommerce/static/authentication/base.js
@@ -1,16 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Code for the color changing of the search button
-    const button = document.getElementById('search-button')
-    const image = document.getElementById('search-image')
-
-    button.onmouseover = () => {
-        image.src = '/static/store/search-black.svg'
-    }
-
-    button.onmouseleave = () => {
-        image.src = '/static/store/search.svg'
-    }
-
     // Code for the favicon
     lightSchemeIcon = document.getElementById('white-theme-favicon');
     darkSchemeIcon = document.getElementById('dark-theme-favicon');
